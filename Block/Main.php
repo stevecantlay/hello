59ad52032ad1273a6501352cc33d2f0d38176ae0
steveCantlay <steve.cantlay@gmail.com>
2016-02-15 11:06:50 +0000

    no message

--- Block/Main.php
+++ Block/Main.php
@@ -6,12 +6,12 @@ class Main extends Template
 {
     protected function _prepareLayout()
     {
-        $this->setMessage('Hello');
+        $this->setMessage('Hello 1.1');
         $this->setName($this->getRequest()->getParam('name'));
     }
 
     public function getGoodbyeMessage()
     {
-        return 'Goodbye World';
+        return 'Goodbye World 1.1';
     }
 }
